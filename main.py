2b2302dea403beba965db1625f9b31a4353b38a7
NZM-April <itsuki2030@live.jp>
2019-02-26 08:30:47 +0900

    add config.yml

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import run
 class Thrim(object):
   def hello(self):
     return 'Hello, Thrim!'
-  def run(self):
-    run.start()
+  def run(self, f='config.yml'):
+    run.start(f)
 
 if __name__ == '__main__':
   fire.Fire(Thrim)
--- run.py
+++ run.py
@@ -1,2 +1,12 @@
-def start():
+import yaml
+
+def start(file):
   print('Thrim start...')
+  f = open(file, "r+")
+  data = yaml.load(f)
+
+  accept = data['input']['accept']
+  for i in range(len(accept)):
+    ip = (accept[i]['ip'])
+    protocol = (accept[i]['protocol'])
+    print('iptables -A INPUT -j ACCEPT -s ' + ip + ' -p ' + protocol)
