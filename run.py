236f9149a43aea4fa905e4a3324bf4292b17a3fa
NZM-April <itsuki2030@live.jp>
2019-02-26 21:37:00 +0900

    fix

--- run.py
+++ run.py
@@ -14,8 +14,8 @@ def run_iptables(data, chain, option):
   option_configs = data[chain][option]
 
   for i in range(len(option_configs)):
-    ip = (option_configs[i]['ip'])
-    protocol = (option_configs[i]['protocol'])
+    ip = option_configs[i]['ip']
+    protocol = option_configs[i]['protocol']
     command = ['iptables', '-A', chain.upper(), '-j', str(option).upper(), '-s', ip, '-p', protocol]
     print('iptables -A ' + chain.upper() + ' -j '+ str(option).upper() + ' -s ' + ip + ' -p ' + protocol)
     subprocess.run(command)
