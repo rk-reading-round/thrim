3334900f83bafd4cdc9859005ebf77e46906e6e1
masahiro.ogawa <masahiro.ogawa@macbook-pro.lo>
2019-03-05 00:18:23 +0900

    add cli class

--- lib/cli/thrim_cli.py
+++ lib/cli/thrim_cli.py
@@ -1,11 +1,24 @@
-import yaml
-import subprocess
-import sys
+from lib.cli.base import Cli
 
-from cli import Cli
+from lib.modules import iptables
 
 class thrimCli(Cli):
-  def __init__(self, *args):
-    self.options = args
-
   def start(self):
+    self.dryrun_thrim(self.data)
+    self.require_confirmation()
+    self.realrun_thrim(self.data)
+
+  def dryrun_thrim(self, data):
+    print('Thrim Dryrun...')
+    iptables.dryrun_iptables(data, 'input', 'accept')
+    iptables.dryrun_iptables(data, 'input', 'drop')
+    iptables.dryrun_iptables(data, 'output', 'accept')
+    iptables.dryrun_iptables(data, 'output', 'drop')
+
+  def realrun_thrim(self, data):
+    print('Thrim start...')
+    iptables.run_iptables(data, 'input', 'accept')
+    iptables.run_iptables(data, 'input', 'drop')
+    iptables.run_iptables(data, 'output', 'accept')
+    iptables.run_iptables(data, 'output', 'drop')
+    print('Thrim complete.')
