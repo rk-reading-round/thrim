bcb8a759abe154735cb94283321ef84e4b9f72b0
NZM-April <itsuki2030@live.jp>
2019-03-20 23:08:58 +0000

    add input-rules read

--- absorb.py
+++ absorb.py
@@ -1,18 +1,27 @@
+#coding: UTF-8
+
 import iptc
 import ipaddress
+import yaml
+import ast
 
 def start():
   print('Import iptables Settings...')
   print("=============================")
-  chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'INPUT')
-  for rule in chain.rules:
+
+  input_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'INPUT')
+  for rule in input_chain.rules:
     print("INPUT")
     display_rule(rule)
-  chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'OUTPUT')
-  for rule in chain.rules:
-    print("OUT")
+
+  output_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'OUTPUT')
+  for rule in output_chain.rules:
+    print("OUTPUT")
     display_rule(rule)
 
+  make_config(input_chain, output_chain)
+
+# debug: delete later
 def display_rule(rule):
   print("=============================")
   print("Target:", rule.target.name)
@@ -32,3 +41,19 @@ def format_address(address):
   else:
     raise TypeError
   return address.with_prefixlen
+
+def read_config(input_chain, output_chain):
+  input_text = ""
+  input_text += "{'input': {"
+  for rule in input_chain.rules:
+    input_text += "\'"
+    input_text += rule.target.name
+    input_text += "\': '', "
+  input_text += "}}"
+  print(input_text)
+  return input_text
+
+def make_config(input_chain, output_chain):
+  input_text = read_config(input_chain, output_chain)
+  with open("absorbed_config.yml", "w") as ac:
+    yaml.dump(ast.literal_eval(input_text), ac, default_flow_style=False)
