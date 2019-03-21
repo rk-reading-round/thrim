19eb17b385b50eefe61e4f77154dccca628d1305
NZM-April <itsuki2030@live.jp>
2019-03-21 12:09:33 +0000

    refactoring

--- absorb.py
+++ absorb.py
@@ -1,5 +1,3 @@
-#coding: UTF-8
-
 import iptc
 import ipaddress
 import yaml
@@ -7,29 +5,17 @@ import ast
 
 def start():
   print('Import iptables Settings...')
-  print("=============================")
-
   input_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'INPUT')
-  for rule in input_chain.rules:
-    print("INPUT")
-    display_rule(rule)
-
   output_chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'OUTPUT')
-  for rule in output_chain.rules:
-    print("OUTPUT")
-    display_rule(rule)
-
   make_config(input_chain, output_chain)
+  print('Import success!!\n$ cat absorbed_config.yml')
 
-# debug: delete later
-def display_rule(rule):
-  print("=============================")
-  print("Target:", rule.target.name)
-  print("Protocol:", rule.protocol)
-  print("Source address:", format_address(rule.src))
-  print("Destination address:", format_address(rule.dst))
-  print("=============================")
+def make_config(input_chain, output_chain):
+  text = read_config(input_chain, output_chain)
+  with open("absorbed_config.yml", "w") as ac:
+    yaml.dump(ast.literal_eval(text), ac, default_flow_style=False)
 
+# format IP address to prefix
 def format_address(address):
   if isinstance(address, (str, ipaddress.IPv4Address)):
     try:
@@ -43,22 +29,25 @@ def format_address(address):
   return address.with_prefixlen
 
 def read_config(input_chain, output_chain):
-  input_text = ""
-  input_text += "{'input': {"
+  text = "{'input': {"
   for rule in input_chain.rules:
-    input_text += "'"
-    input_text += rule.target.name.lower()
-    input_text += "': "
-    input_text += "[{'ip': '"
-    input_text += format_address(rule.src)
-    input_text += "'}, {'protocol': '"
-    input_text += rule.protocol
-    input_text += "'}], "
-  input_text += "}}"
-  print(input_text)
-  return input_text
-
-def make_config(input_chain, output_chain):
-  input_text = read_config(input_chain, output_chain)
-  with open("absorbed_config.yml", "w") as ac:
-    yaml.dump(ast.literal_eval(input_text), ac, default_flow_style=False)
+    text += "'"
+    text += rule.target.name.lower()
+    text += "': [{'ip': '"
+    text += format_address(rule.src)
+    text += "', 'protocol': '"
+    text += rule.protocol
+    text += "'}], "
+  text = text.rstrip(', ')
+  text += "}, 'output': {"
+  for rule in output_chain.rules:
+    text += "'"
+    text += rule.target.name.lower()
+    text += "': [{'ip': '"
+    text += format_address(rule.src)
+    text += "', 'protocol': '"
+    text += rule.protocol
+    text += "'}], "
+  text = text.rstrip(', ')
+  text += "}}"
+  return text
