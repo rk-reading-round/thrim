ee87861403c12e44be38a343d9d6b1a74c9fc920
masahiro.ogawa <masahiro.ogawa@macbook-pro.lo>
2019-04-01 22:00:42 +0900

    add iptables command

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ def start(file):
   f = open(file, "r+")
   data = yaml.load(f)
   dryrun_thrim(data)
-  # require_confirmation()
-  # realrun_thrim(data)
+  require_confirmation()
+  realrun_thrim(data)
 
 def require_confirmation():
   while True:
@@ -32,14 +32,18 @@ def dryrun_thrim(data):
 
 def realrun_thrim(data):
   print('Thrim start...')
-  try:
-    run_iptables(data, 'input', 'accept')
-    run_iptables(data, 'input', 'drop')
-    run_iptables(data, 'output', 'accept')
-    run_iptables(data, 'output', 'drop')
-    print('Thrim complete.')
-  except FileNotFoundError:
-    print('[Error] command iptables not found')
+  opt_pattern = {}
+  for i in data:
+    opt_pattern[i] = list(data[i].keys())
+
+  for i in opt_pattern:
+    chain = i
+    for j in range(len(opt_pattern[i])):
+      target = opt_pattern[i][j]
+      try:
+        run_iptables(data, chain, target)
+      except FileNotFoundError:
+        print('[Error] command iptables not found')
 
 def dryrun_iptables(data, chain, target):
   rules = data[chain][target]
@@ -51,17 +55,19 @@ def dryrun_iptables(data, chain, target):
       command.append(i[j])
     print(' '.join(map(str, command)))
 
-def run_iptables(data, command, option):
-  option_configs = data[command][option]
-
-  for i in range(len(option_configs)):
-    ip = option_configs[i]['ip']
-    protocol = option_configs[i]['protocol']
-    print('[Running] iptables -A ' + command.upper() + ' -j '+ str(option).upper() + ' -s ' + ip + ' -p ' + protocol)
+def run_iptables(data, chain, target):
+  rules = data[chain][target]
+  opt_dict = {'src': '-s', 'protocol': '-p', 'in_interface': '-i', 'dport': '--dport'}
+  for i in rules:
+    command = ['iptables -A', chain.upper(), '-j', target.upper()]
+    for j in i:
+      command.append(opt_dict[j])
+      command.append(i[j])
+    print(' '.join(map(str, command)))
 
     rule = iptc.Rule()
-    rule.src = ip
-    rule.protocol = protocol
-    rule.create_target(str(option).upper())
-    chain = iptc.Chain(iptc.Table('filter'), command.upper())
-    chain.insert_rule(rule)
+    for j in i:
+      setattr(rule, j, i[j])
+    rule.create_target(str(target).upper())
+    table_chain = iptc.Chain(iptc.Table('filter'), chain.upper())
+    table_chain.insert_rule(rule)
