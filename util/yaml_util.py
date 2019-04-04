5a6515a3f128b0dca88468d1dfcdc2dec68a1bf5
NZM-April <itsuki2030@live.jp>
2019-04-04 23:06:37 +0000

    add def data2_minus_data1

--- util/yaml_util.py
+++ util/yaml_util.py
@@ -19,3 +19,18 @@ def yaml_sort(yaml_file):
 
   with open(yaml_file, "w") as yf:
     yaml.dump(ast.literal_eval(sorted_config), yf, default_flow_style=False)
+
+def yaml_diff(yaml_file1, yaml_file2):
+  data1 = yaml.load(open(yaml_file1, "r+"))
+  data2 = yaml.load(open(yaml_file2, "r+"))
+  diff_input_accept = data2_minus_data1(data1, data2, 'input', 'accept')
+  diff_input_drop = data2_minus_data1(data1, data2, 'input', 'drop')
+  diff_output_accept = data2_minus_data1(data1, data2, 'output', 'accept')
+  diff_output_drop = data2_minus_data1(data1, data2, 'output', 'drop')
+
+def data2_minus_data1(data1, data2, command, option):
+  data1_list = [v.get('ip') for v in data1[command][option]]
+  data2_list = [v.get('ip') for v in data2[command][option]]
+  return list(set(data2_list) - set(data1_list))
+
+yaml_diff('config.yml', '.state.yml')
