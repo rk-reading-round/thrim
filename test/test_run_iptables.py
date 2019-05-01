836050de73211ec4d646ced5b27cd28596fa15d4
MSHR-Dec <masahiro.ogawa@macbook-pro>
2019-05-01 17:22:04 +0900

    fix indent

--- test/test_run_iptables.py
+++ test/test_run_iptables.py
@@ -5,36 +5,36 @@ import subprocess
 from run import run_iptables
 
 class TestRunIptables(unittest.TestCase):
-	def tearDown(self):
-		subprocess.run(["iptables", "--flush"])
+  def tearDown(self):
+    subprocess.run(["iptables", "--flush"])
 
-	def test_thrim_run(self):
-		#sample data
-		data = {
+  def test_thrim_run(self):
+    #sample data
+    data = {
       'input': {
         'accept': [
           {'src': '22.22.22.22/32',
-           'protocol': 'tcp',
-           'in_interface': 'eth0'
+            'protocol': 'tcp',
+            'in_interface': 'eth0'
           }
         ]
       }
     }
-		chain = "input"
-		target = "accept"
+    chain = "input"
+    target = "accept"
 
-		#testrun run_iptables
-		run_iptables(data, chain, target)
-		actual = []
-		table = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'INPUT')
-		actual.append(table.rules[0].target.name)
-		actual.append(table.rules[0].src)
-		actual.append(table.rules[0].protocol)
-		actual.append(table.rules[0].in_interface)
+    #testrun run_iptables
+    run_iptables(data, chain, target)
+    actual = []
+    table = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'INPUT')
+    actual.append(table.rules[0].target.name)
+    actual.append(table.rules[0].src)
+    actual.append(table.rules[0].protocol)
+    actual.append(table.rules[0].in_interface)
 
-		expect = ['ACCEPT', '22.22.22.22/255.255.255.255', 'tcp', 'eth0']
+    expect = ['ACCEPT', '22.22.22.22/255.255.255.255', 'tcp', 'eth0']
 
-		self.assertEqual(expect, actual)
+    self.assertEqual(expect, actual)
 
 if __name__ == "__main__":
-  unittest.main()
+unittest.main()
